759a56a6de3d187574a155b28448bad642a12b5f
wildcat222 <wildcat222@naver.com>
2024-08-01 21:48:42 +0900

    chap04_level2_normal5

--- practice_Java/src/chap04_control_flow/level3_hard3.java
+++ practice_Java/src/chap04_control_flow/level3_hard3.java
@@ -27,15 +27,21 @@ public class level3_hard3 {
 
         if (n <= 2) {
             System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
-        } else {
-            for (int i = 2; i < n; i++) {
-                if (n % i == 0) {
-                    System.out.println("소수가 아니다.");
-                } else {
-                    System.out.println("소수다.");
-                }
+            System.out.println("2보다 큰 정수를 하나 입력하세요 : ");
+            n = scanner.nextInt();
+        }
 
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                count++;
             }
+        }
+
+        if (count >= 1) {
+            System.out.println("소수가 아니다.");
+        } else {
+            System.out.println("소수다.");
         }
 
     }
