e58ef648f91e6478f5dd1c3f9c7de94bdcf2b586
wildcat222 <wildcat222@naver.com>
2024-08-01 20:16:43 +0900

    chap04_level1_basic3

--- practice_Java/src/chap04_control_flow/level1_basic3.java
+++ practice_Java/src/chap04_control_flow/level1_basic3.java
@@ -6,7 +6,7 @@ public class level1_basic3 {
          *
          * -- 출력 예시 --
          * 1부터 10까지의 합 : 55
-         * */
+          */
         int sum = 0;
         for (int i = 1; i <= 10; i++) {
             sum += i;
