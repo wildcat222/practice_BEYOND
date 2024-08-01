fb600517dbddcc43f03b787220ea213e04f5aa26
wildcat222 <wildcat222@naver.com>
2024-08-01 20:50:19 +0900

    chap04_level2_normal4

---
+++ practice_Java/src/chap04_control_flow/level2_normal4.java
@@ -0,0 +1,15 @@
+package chap04_control_flow;
+
+public class level2_normal4 {
+    public static void main(String[] args) {
+        /* 반복문을 이용하여 알파벳 소문자 'a'부터 'z'까지를 개행 없이 차례로 출력하세요
+         *
+         * -- 출력 예시 --
+         * abcdefghijklmnopqrstuvwxyz
+         * */
+
+        for (int i = 'a'; i <= 'z'; i++) {
+            System.out.print((char) i);
+        }
+    }
+}
