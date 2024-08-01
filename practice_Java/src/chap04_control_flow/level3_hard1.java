2401b7f0299a8f62d996006383d851d8cdcc4f00
wildcat222 <wildcat222@naver.com>
2024-08-01 17:36:40 +0900

    chap04_level3_hard1

---
+++ practice_Java/src/chap04_control_flow/level3_hard1.java
@@ -0,0 +1,43 @@
+package chap04_control_flow;
+
+import java.util.Scanner;
+
+public class level3_hard1 {
+    public static void main(String[] args) {
+        /* 두 개의 정수를 입력 받아 변수에 저장하고,
+         * 연산기호(+, -, *, /, %)를 입력 받아 해당 연산의 수행 결과를 출력하세요.
+         * 단, 준비된 연산기호 외의 문자를 입력하는 경우 "입력하신 연산은 없습니다. 프로그램을 종료합니다." 출력 후 프로그램 종료
+         *
+         * -- 입력 예시 --
+         * 첫 번째 정수 : 4
+         * 두 번째 정수 : 3
+         * 연산 기호를 입력하세요 : +
+         *
+         * -- 출력 예시 --
+         * 4 + 3 = 7
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("첫 번째 정수를 입력하세요. : ");
+        int first = scanner.nextInt();
+        System.out.print("두 번째 정수를 입력하세요. : ");
+        int second = scanner.nextInt();
+        System.out.print("연산 기호를 입력하세요. : ");
+        char op = scanner.next().charAt(0);
+
+        int result = 0;
+
+        switch (op) {
+            case '+' : result = first + second; break;
+            case '-' : result = first - second; break;
+            case '*' :
+            case 'x' : result = first * second; break;
+            case '/' : result = first / second; break;
+            case '%' : result = first % second; break;
+            default :
+                System.out.println("입력하신 연산은 없습니다. 프로그램을 종료합니다.");
+        }
+        System.out.println(first + " + " + second + " = " + result);
+
+    }
+}
